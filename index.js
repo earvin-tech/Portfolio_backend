48baf31c546b4824b64b30c7781a0b85fd675d38
earvin-tech <wsl@Earvin>
2025-04-22 20:01:57 +1000

    Fixed CORS

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["earvinporfolio2.netlify.app"],
+}));
 app.use(express.json());
 
 // Sample route
